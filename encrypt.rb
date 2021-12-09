23fa567adf39bb31092b63151afb4c7cbcdcdd06
Christine Schorn <cschorn91@gmail.com>
2021-12-09 22:05:49 +1100

    Livecode letter_changer encrypt method

---
+++ encrypt.rb
@@ -0,0 +1,14 @@
+def encrypt(text)
+   # 1. Define an array with the 26 letters of the alphabet (in capital letters)
+	alphabet = ("A".."Z").to_a
+	# 2. Upcase the text and split the text in a `letters` array
+  text_split_in_letters = text.upcase.split("")
+	# 3. Iterate over letters, for each letter, test its inclusion in the alphabet
+  encrypted_letter_array = text_split_in_letters.map do |letter|
+    # 4. Map the letter to its left-shift or to itself
+    index = alphabet.index(letter)
+    index.nil? ? letter : alphabet[index - 3]
+  end
+  # 5. Join the mapped array and return it
+  encrypted_letter_array.join
+end
